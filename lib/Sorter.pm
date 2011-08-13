db223824e86f66d84a2b7be426bb0cfcb0c7ce35
giginet <giginet.net@gmail.com>
2011-08-14 06:07:01 +0900

    Quick Sort実装

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,4 +1,7 @@
-# implement this
+# 
+# Sorter.pm
+# created by giginet on 2011/8/14
+# 
 package Sorter;
 use strict;
 use warnings;
@@ -7,8 +10,8 @@ sub new{
   my $class = shift;
   my $self = {
     values => \@_,
-  };
-  return bless $self, $class;
+};
+return bless $self, $class;
 }
 
 sub set_values{
@@ -23,9 +26,30 @@ sub get_values{
 
 sub sort{
   my $self = shift;
-  my @sorted = sort { $a <=> $b } @{$self->{values}};
-  $self->{values} = \@sorted;
+  my @values = $self->get_values;
+  my $length = @values;
+  $self->_quick_sort(0, $length-1);
+}
 
+sub _quick_sort(){
+  my ($self, $begin, $end) = @_;
+  my @values = $self->get_values;
+  my ($i, $j, $pivot);
+  if ($begin < $end) {
+    ($i, $j) = ($begin, $end);
+    $pivot = $values[($begin+$end)/2];
+    while($i <= $j){
+      ++$i while ($values[$i] < $pivot);
+      --$j while ($values[$j] > $pivot);
+      if ($i <= $j) {
+        ($values[$i], $values[$j]) = ($values[$j], $values[$i]);
+        ++$i, --$j;
+      }
+    }
+    $self->set_values(@values);
+    $self->_quick_sort($begin, $j);
+    $self->_quick_sort($i, $end);
+  }
 }
 
 1;
