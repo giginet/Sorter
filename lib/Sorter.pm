98d039e8930009eddf0adc91951dc6d1855321c4
giginet <giginet.net@gmail.com>
2011-08-14 05:07:36 +0900

    とりあえずテストは通るように

--- lib/Sorter.pm
+++ lib/Sorter.pm
@@ -1,1 +1,31 @@
 # implement this
+package Sorter;
+use strict;
+use warnings;
+
+sub new{
+  my $class = shift;
+  my $self = {
+    values => \@_,
+  };
+  return bless $self, $class;
+}
+
+sub set_values{
+  my $self = shift;
+  $self->{values} = \@_;
+}
+
+sub get_values{
+  my $self = shift;
+  return @{$self->{values}};
+}
+
+sub sort{
+  my $self = shift;
+  my @sorted = sort { $a <=> $b } @{$self->{values}};
+  $self->{values} = \@sorted;
+
+}
+
+1;
